04f638d9ba235c3a8b70a60c540faef37dd65b9b
Rani Bhattacharjee <rani.bhattacharjee29@gmail.com>
2025-12-16 16:40:10 +0530

    Refactor Firebase initialization with environment variable
    Update Firebase initialization to use environment variable for service account.

--- gdg_backend/firebase/init.py
+++ gdg_backend/firebase/init.py
@@ -1,9 +1,15 @@
+import os
+import json
 import firebase_admin
 from firebase_admin import credentials, firestore
 
-cred = credentials.Certificate("firebase/serviceAccountKey.json")
-
 if not firebase_admin._apps:
+    service_account = os.environ.get("FIREBASE_SERVICE_ACCOUNT")
+
+    if not service_account:
+        raise ValueError("FIREBASE_SERVICE_ACCOUNT not set")
+
+    cred = credentials.Certificate(json.loads(service_account))
     firebase_admin.initialize_app(cred)
 
 db = firestore.client()
