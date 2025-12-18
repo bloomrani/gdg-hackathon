716fbd579a940e6b6d0ba97e03e4a6ceb5e75286
Ritankar Bose <ritankarbose100@gmail.com>
2025-12-18 17:55:58 +0530

    Delete gdg_frontend directory

--- gdg_frontend/src/pages/StudentDashboard.jsx
+++
@@ -1,9 +0,0 @@
-export default function StudentDashboard() {
-  return (
-    <div className="p-6">
-      <h1 className="text-2xl font-semibold">
-        Student Dashboard
-      </h1>
-    </div>
-  );
-}
