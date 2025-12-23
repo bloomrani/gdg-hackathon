b9284fcebf515e588e5131399b96d3d258512a56
Ritankar Bose <ritankarbose100@gmail.com>
2025-12-23 19:36:17 +0530

    Add email service to send emails via SMTP
    Implement email sending functionality using SMTP.

--- gdg_backend/utils/email_service.py
+++ gdg_backend/utils/email_service.py
@@ -1,1 +1,28 @@
 
+import smtplib
+from email.mime.text import MIMEText
+from email.mime.multipart import MIMEMultipart
+import os
+
+
+SMTP_SERVER = os.getenv("SMTP_SERVER")
+SMTP_PORT = int(os.getenv("SMTP_PORT"))
+SENDER_EMAIL = os.getenv("SENDER_EMAIL")
+SENDER_PASSWORD = os.getenv("SENDER_PASSWORD")
+
+
+def send_email(to_emails, subject, html_content):
+    if not to_emails:
+        return
+
+    msg = MIMEMultipart()
+    msg["From"] = f"Campus Issue Portal <{SENDER_EMAIL}>"
+    msg["To"] = ", ".join(to_emails)
+    msg["Subject"] = subject
+
+    msg.attach(MIMEText(html_content, "html"))
+
+    with smtplib.SMTP(SMTP_SERVER, SMTP_PORT) as server:
+        server.starttls()
+        server.login(SENDER_EMAIL, SENDER_PASSWORD)
+        server.send_message(msg)
